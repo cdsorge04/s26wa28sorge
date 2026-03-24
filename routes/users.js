7aba1e8d8a0312fcf179d60d346a994a5d38679f
cdsorge04 <cdsorge04@gmail.com>
2026-03-23 19:57:11 -0500

    updated users endpoint with math

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (var1 > var2) {
+    var1 = var1 - var2;
+  } else {
+    var2 = var2 - var1;
+  }
+
+  res.send(`Values are: ${var1} and ${var2}`);
 });
 
 module.exports = router;
+
+let var1 = 10;
+let var2 = 15;
